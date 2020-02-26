f7688575ecf2a2bbce77823d4fb7b287a2770c06
mathekaJosef <mathekajosef571@gmail.com>
2020-02-26 16:12:28 +0300

    add first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import React, { Component, lazy, Suspense } from 'react'
+// import TodoApp from './components/TodoApp'
+// import LazyLoading from './components/LazyLoading'
+const LazyLoading = lazy(() => import('./components/LazyLoading'))
+
+export class App extends Component {
+  render() {
+    return (
+      
+        <div style={{margin:10}}>
+          <div>another component</div>
+          <Suspense fallback={<div>Loading...</div>}>
+            <LazyLoading/>
+          </Suspense>
+        </div>
+      
+    )
+  }
+}
+
+export default App
